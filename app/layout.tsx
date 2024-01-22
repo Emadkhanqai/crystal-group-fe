a1e4aae0625593fe4a79cf25c999a29e3dc2c05d
Emad Hassan Khan <emadkhanqai@gmail.com>
2024-01-22 15:55:01 +0500

    prepared few pages with api integrations

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +17,25 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className + ' px-10'}>{children}</body>
+      <body className={inter.className + ' px-10'}>
+        <header className="text-gray-600 body-font">
+          <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+            <Link href='/' className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
+                <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+              </svg>
+              <span className="ml-3 text-xl">Crystal Panda</span>
+            </Link>
+            <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
+              <Link href="/menu" className="mr-5 hover:text-gray-900">Menu</Link>
+              <Link href="/favorites" className="mr-5 hover:text-gray-900">My Favorites</Link>
+              <Link href="/meal-generator" className="mr-5 hover:text-gray-900">Meal Generator</Link>
+              <Link href="/about-me" className="mr-5 hover:text-gray-900">About Me</Link>
+              <Link href="/contact" className=" hover:text-gray-900">Contact</Link>
+            </nav>
+          </div>
+        </header>
+        {children}</body>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -14,28 +14,10 @@ async function getData(): Promise<CategoriesResponse> {
 }
 
 export default async function Home() {
-
   const data = await getData();
 
   return (
     <>
-      <header className="text-gray-600 body-font">
-        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-          <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
-              <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-            </svg>
-            <span className="ml-3 text-xl">Crystal Panda</span>
-          </a>
-          <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-            <Link href="/" className="mr-5 hover:text-gray-900">Menu</Link>
-            <Link href="/" className="mr-5 hover:text-gray-900">My Favorites</Link>
-            <Link href="/" className="mr-5 hover:text-gray-900">Meal Generator</Link>
-            <Link href="/" className=" hover:text-gray-900">About Me</Link>
-          </nav>
-        </div>
-      </header>
-
       <section className="text-gray-600 body-font">
         <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
           <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
@@ -54,17 +36,14 @@ export default async function Home() {
           </div>
         </div>
       </section>
-
       <section className="text-gray-600 body-font">
         <div className="container px-5 py-6 mx-auto">
           <h1 className="text-3xl mb-10 font-extrabold leading-none tracking-tight text-gray-900 md:text-3xl lg:text-4xl">Our Specialities</h1>
-          
-            <Specialities category={data} />
-           
+          <Specialities category={data} />
         </div>
       </section>
       <br></br>
-      <Footer></Footer>
+      <Footer />
     </>
 
   )
